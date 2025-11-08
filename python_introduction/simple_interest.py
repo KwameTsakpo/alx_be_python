594e08cb3e8b86a11a7b2e5c675f3ce0fd9f29d6
KwameTsakpo <godfredtsakpo@gmail.com>
2025-11-08 09:22:55 +0000

    Refactor variable names in simple interest calculation
    Renamed variables to more descriptive names (principal, rate, time, interest) for clarity and updated the calculation accordingly.

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
 # Programme to calculate simple interest
 
-P = 2000 #Principal
+principal = 2000 #Principal
 
-R = 20/100 #Rate(100)
+rate = 20/100 #Rate(100)
 
-T = 2 #Time(years)
+time = 2 #Time(years)
 
-I = P * R * T #Interest
-print("The simple interest is: ",I )
+interest = time * rate * principal #Interest
+print("The simple interest is: ",interest)
