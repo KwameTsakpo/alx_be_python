c2ba1caac1c606449e7b29113065e0854ed9aebb
KwameTsakpo <godfredtsakpo@gmail.com>
2025-11-08 01:33:05 +0000

    Add basic arithmetic operations script
    Introduces basic_operations.py to perform and print addition, subtraction, and multiplication of two integers.

---
+++ basic_operations.py
@@ -0,0 +1,20 @@
+number1 = int(10)
+number2 = int(5)
+
+operation = "Addition"
+
+result = number1 + number2
+
+print(operation,'of ',number1,' and ',number2,' is ', result)
+
+operation = "difference"
+
+result = number1 - number2
+
+print(operation,'of ',number1,' and ',number2,' is ', result)
+
+operation = "product"
+
+result = number1 * number2
+
+print(operation,'of ',number1,' and ',number2,' is ', result)
