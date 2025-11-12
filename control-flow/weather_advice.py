c3752054af7cbfd8ac340e2d8ec7384bb4d85a5b
KwameTsakpo <godfredtsakpo@gmail.com>
2025-11-12 05:16:39 +0000

    updates

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,10 @@
-weather = input("What is the weather like today? (sunny/rainy/cold)")
+weather = input("What's the weather like today? (sunny/rainy/cold):")
 
-match weather:
-    case "sunny":
-        print(f'Wear a t-shirt and sunglasses')
-    case "rainy":
-        print(f"Don't forget your umbrella and a raincoat")
-    case "cold":
-        print(f'Make sure to wear a warm coat and a scarf.')
-    case _:
-        print(f"Sorry, I don't have recommendations for this weather.")
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses")
+elif weather == "rainy":
+    print("Don't forget your umbrella and a raincoat")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
