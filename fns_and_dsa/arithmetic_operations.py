d0135840e986e60e1f94a5f643a21fc51899b35a
KwameTsakpo <godfredtsakpo@gmail.com>
2025-11-22 18:48:14 +0000

    Simplified function for perfom_operation()

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,32 +1,18 @@
+def perform_operation(num1, num2, operation):
 
-def addition(num1, num2):
-    return(num1 + num2)
+    if operation == "add":
+        return num1 + num2
 
-def subtraction(num1,num2):
-    return(num1 - num2)
+    elif operation == "subtract":
+        return num1 - num2
 
-def multiplication(num1, num2):
-    return(num1 * num2)
+    elif operation == "multiply":
+        return num1 * num2
 
-def division(num1,num2):
-    if num2 == 0:
-        return("Sorry division by zero is undefined")
-    else: 
-        return(num1/num2)
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
 
-def perform_operation(num1,num2,operation):
-    
-    if(operation == "add"):
-        return(addition(num1,num2))
-    
-    elif(operation == "subtract"):
-        return(subtraction(num1,num2))
-    
-    elif(operation == "multiply"):
-        return(multiplication(num1,num2))
-    
-    elif(operation == "divide"):
-        return(division(num1,num2))
-    
     else:
-        return("Invalid input Try Again")
+        return "Error: Unsupported operation."
